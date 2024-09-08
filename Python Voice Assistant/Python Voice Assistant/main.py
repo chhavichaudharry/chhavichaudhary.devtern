4581074e9a2272585b669c0bc7c11cb5cdb74041
chhavichaudharry <139902610+chhavichaudharry@users.noreply.github.com>
2024-09-08 08:13:43 +0530

    Update main.py

--- Python Voice Assistant/Python Voice Assistant/main.py
+++ Python Voice Assistant/Python Voice Assistant/main.py
@@ -1,83 +1,59 @@
-import speech_recognition as sr
+import speech_recognition as sr 
 import webbrowser
-import pyttsx3
+import pyttsx3  
 import musicLibrary
 import requests
-from gtts import gTTS
-import pygame
-import os
 
-# pip install pocketsphinx and google sr
+#pip install pocketsphinx and google sr
 
 recognizer = sr.Recognizer()
-engine = pyttsx3.init() 
+engine = pyttsx3.init()
 newsapi = "c8320e29b762415a9a065c28a1603ab5"
 
-def speak_old(text):
+def speak(text):
     engine.say(text)
     engine.runAndWait()
-
-def speak(text):
-    tts = gTTS(text)
-    tts.save('temp.mp3') 
-
-    # Initialize Pygame mixer
-    pygame.mixer.init()
-
-    # Load the MP3 file
-    pygame.mixer.music.load('temp.mp3')
-
-    # Play the MP3 file
-    pygame.mixer.music.play()
-
-    # Keep the program running until the music stops playing
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-    
-    pygame.mixer.music.unload()
-    os.remove("temp.mp3") 
-    
-
+        
 def process_command(c):
-    if "open google" in c.lower():
-        webbrowser.open("https://google.com")
-    elif "open facebook" in c.lower():
-        webbrowser.open("https://facebook.com")
+    if "open brave" in c.lower():
+        webbrowser.open("https://brave.com")
+    elif "open whatsapp" in c.lower():
+        webbrowser.open("https://whatsapp.com")
     elif "open youtube" in c.lower():
-        webbrowser.open("https://youtube.com")
+        webbrowser.open("https://youtube.com") 
     elif "open linkedin" in c.lower():
-        webbrowser.open("https://linkedin.com")
+        webbrowser.open("https://linkedin.com") 
     elif c.lower().startswith("play"):
         song = c.lower().split(" ")[1]
         link = musicLibrary.music[song]
         webbrowser.open(link)
-
+ 
     elif "news" in c.lower():
-        r = requests.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=c8320e29b762415a9a065c28a1603ab5")
+        r = requests.get("https://newsapi.org/v2/top-headlines?country=ina&apiKey=c8320e29b762415a9a065c28a1603ab5")
         if r.status_code == 200:
             # Parse the JSON response
             data = r.json()
-            
+ 
             # Extract the articles
-            articles = data.get('articles', [])
-            
-            # speak the headlines
-            for article in articles:
-                speak(article['title'])
+            articles = data.get('articles', [])        
 
+            # Print the headlines
+            for article in articles:
+               speak(article['title'])
 
 if __name__ == "__main__":
     speak("Initializing Zara....")
+    print("Initializing Zara...")
     while True:
-        # Listen for the wake word "Jarvis"
+        # Listen for the wake word "zara"
         # obtain audio from the microphone
         r = sr.Recognizer()
-         
+        
         print("recognizing...")
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=5 , phrase_time_limit=3)
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
             word = r.recognize_google(audio)
             if(word.lower() == "zara"):
                 speak("Ya")
@@ -89,6 +65,6 @@ if __name__ == "__main__":
 
                     process_command(command)
 
-
         except Exception as e:
-            print("Error; {0}".format(e))
+            print("Error; {0}".format(e))  
+
